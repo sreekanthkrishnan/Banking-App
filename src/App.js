dd8928c3d5352797f6a0f50fe0cda4d66c0b1712
SreekanthkrishnangitlabzE <sreekanth.k@giglabz.com>
2023-07-20 13:00:41 +0530

    latest

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MainRouter from "./Components/Routers/MainRouter";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+	return (
+		<div className="App">
+			<MainRouter />
+		</div>
+	);
+}
+
+export default App;
